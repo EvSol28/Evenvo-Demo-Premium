4817bb380c081505cc44ea8f0a1d88a98df0e376
Neovoo <contact@moovoo.org>
2025-12-28 00:16:02 +0100

    Add debug for private key format

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ try {
       type: "service_account",
       project_id: process.env.FIREBASE_PROJECT_ID || "evenvo-ba568",
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -82,6 +82,8 @@ try {
       client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
       universe_domain: "googleapis.com"
     };
+
+    console.log('Clé privée (premiers 50 caractères):', process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.substring(0, 50) + '...' : 'undefined');
 
     firebaseConfig = {
       credential: admin.credential.cert(serviceAccount),
